0f5fc663f2c8d10eb04f24ecaf484903535dc664
HarshSingh998 <hrshalsingh@gmail.com>
2026-09-12 21:32:19 +0530

    Class - 3

--- Class/Class - 5/For-Loop .py
+++ Class/Class - 5/For-Loop .py
@@ -1,34 +1,4 @@
 
-# Loops -> Kisi Ek block ko baar baar repeat krna hai to loops ka use krte h. 
-
-""" Imagine printing "Hello" 100 times.
- Without loops: 100 lines of code. 
- With a loop: just 2 lines.
- Loops let you repeat a block of code without rewriting it. """
-
-
-# Python has 2 types of loops: "for" and "while"
-
-
-
-# Kya hota hai "for" loop and "while" loop
-""" The Bucket Analogy 🪣 ->
-
-🔢 FOR loop — known iterations
-Transfer exactly 4 mugs. You know the count → use for.
-
-🔁 WHILE loop — known condition
-Transfer until bucket is empty. You don't know the count, 
-but you know when to stop → use while  """
-
-
-
-
-
-
-
-
-
 
 # 🔢 For Loop ( For Loop with Numbers ) ->
 
