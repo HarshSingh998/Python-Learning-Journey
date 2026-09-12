72c2dae8bbdcfdc15cda126084936c820b7a00b3
HarshSingh998 <hrshalsingh@gmail.com>
2026-09-12 21:27:06 +0530

    Class - 1

---
+++ Class/Class - 1/Comments & Variables .py
@@ -0,0 +1,49 @@
+# For Single Line Comment We Used Hastag (#) 
+#This Line Below is used for printing
+
+print ("Hello NYC Students")
+
+
+
+# For Multi Line Comment We Use ( """ """ )
+""" Hello How Are You 
+I Am Harsh Singh 
+I am From Indore """
+
+
+
+
+# For Storing We Will Use - Variables 
+
+""" Anything Can become variable but there are some rule that you have to follow and
+bas equal to sign lgna padega """
+
+""" There Are 3 Rule to follow variables - 
+1name -> can't start with a number
+my name -> no space allowed
+my-name -> no special characters { Not Use -> !@#$%^&*() }  ( except underscore ) """
+
+
+
+
+
+
+# Example Of anything can become variables 
+sher = 12
+chacha = 54
+hellono1 = 41
+print (sher)
+print (chacha)
+print (hellono1)
+
+
+
+
+
+
+""" There are three cases of naming conventions -> 
+Camel Case - myVariableName
+Pascal Case - MyVariableName
+Snake Case - my_variable_name """
+# But Python Prefers - Snake Case
+
