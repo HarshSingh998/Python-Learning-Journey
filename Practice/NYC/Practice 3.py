29137ca3afb334e556b31723fffa4f616d9047ef
HarshSingh998 <hrshalsingh@gmail.com>
2026-09-13 14:56:27 +0530

    Practice - 3

--- Practice/NYC/Practice 3.py
+++ Practice/NYC/Practice 3.py
@@ -1,5 +1,5 @@
 
-# Class - 6
+# Class - 5
 # While Loop Questions 
 
 
@@ -9,4 +9,53 @@ Question 3 -> Check if a number is palindromic (equal to its reverse). """
 
 
 
+
+
+
+# Separate each digit of a number and print on a new line.
+
+a = int(input("Please Tell Your Number :- "))
+
+while a > 0:
+    print (a % 10)
+    a = a // 10
+
+
+
+
+
+
+
+# Accept a number and print its reverse.
+
+a = int(input("Please Tell Your Number :- "))
+
+rev = 0
+
+while a > 0:
+    rev = rev * 10 + a % 10
+    a = a // 10 
+print (rev)
+
+
+
+
+
+
+
+# Check if a number is palindromic (equal to its reverse).
+
+a = int(input("Please Tell Your Number :- "))
+copy = a
+rev = 0
+
+while a > 0:
+    rev = rev * 10 + a % 10
+    a = a // 10 
+print (rev)
+
+if rev == copy:
+    print("Palindrome")
+else:
+    print("Not a Palindrome")
 
