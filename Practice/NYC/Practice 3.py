d45d0be8304528289dc8077534fe0d558c673ad3
HarshSingh998 <hrshalsingh@gmail.com>
2026-09-13 14:25:31 +0530

    Practice - 3

--- Practice/NYC/Practice 3.py
+++ Practice/NYC/Practice 3.py
@@ -3,5 +3,10 @@
 # While Loop Questions 
 
 
+"""  Question 1 -> Separate each digit of a number and print on a new line.
+Question 2 -> Accept a number and print its reverse.
+Question 3 -> Check if a number is palindromic (equal to its reverse). """
+
+
 
 
